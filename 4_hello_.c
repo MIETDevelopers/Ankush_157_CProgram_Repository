cd7016d58dcfd21d5dc0a0bea59b0ad0b88d59df
unknown <2020a1r157@mietjammu.in>
2021-03-18 00:12:26 +0530

    hello world program

--- 4_hello_.c
+++ 4_hello_.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-int main(){
-	printf("Hello world");
-	return 0;
+#include<stdio.h>//preprocessor to include standard input or output header file
+int main(){//Where the execution of program start from main
+	printf("Hello world");//printf uses for to print output
+	return 0;//return statement 
 }
