40f8f6ec6ac3849d9c45c569c2b0a7d5dbdb5be1
unknown <2020a1r157@mietjammu.in>
2021-03-18 21:35:28 +0530

    hello world program

---
+++ 3_hello_.c
@@ -0,0 +1,5 @@
+#include<stdio.h>//preprocessor directive to include standard input or output header file
+int main(){//Where the execution of program start from main
+	printf("Hello world");//printf uses for to print output
+	return 0;//return statement 
+}
