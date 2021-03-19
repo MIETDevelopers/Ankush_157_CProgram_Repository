9afcaa7d202fa6223ae594a7c5344a9293c1bb3e
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-19 13:13:08 +0530

    Update 5_even_odd_number_.c

--- 5_even_odd_number_.c
+++ 5_even_odd_number_.c
@@ -4,11 +4,11 @@ int main(){//where the execution start from the main
 	printf("Enter the number\n");
 	scanf("%d", &num);//scanf used for to take input for user
 	if (num%2==0)//num are divisible by 2
-	{printf("This number is prime=%d\n",num);//if the num is divisible by 2 print if condition
+	{printf("This number is even=%d\n",num);//if the num is divisible by 2 print if condition
 	
 	}
 	else{
-		printf("This number is not prime=%d\n",num);//if the num is not divisible by 2 print else condition
+		printf("This number is odd=%d\n",num);//if the num is not divisible by 2 print else condition
 	}
 	return 0;//return statement
 }
