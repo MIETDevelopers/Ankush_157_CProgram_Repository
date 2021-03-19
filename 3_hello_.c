4939bf07143d4b33c72ef4be62b52c9db497e908
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-19 09:42:49 +0530

    Update 3_hello_.c

--- 3_hello_.c
+++ 3_hello_.c
@@ -1,5 +1,5 @@
 #include<stdio.h>//preprocessor directive to include standard input or output header file
 int main(){//Where the execution of program start from main
-	printf("Hello world");//printf uses for to print output
+	printf("Hello world");//print the output
 	return 0;//return statement 
 }
