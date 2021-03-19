d443c79109acaf05cebb6582b044dc91cc41ab9b
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-19 09:36:45 +0530

    Delete 4_hello_.c

--- 4_hello_.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>//preprocessor directive to include standard input or output header file
-int main(){//Where the execution of program start from main
-	printf("Hello world");//printf uses for to print output
-	return 0;//return statement 
-}
