655304a97577513dce4075b67cf6d769e67161f7
unknown <2020a1r157@mietjammu.in>
2021-03-21 09:38:31 +0530

    Find factorial number using recursion

---
+++ 21_factorial_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+ 
+int factorial(int);
+ 
+int main()//where the execution start from the main function
+{
+    int num;//declare num variable
+    int result;//declare result variable
+ 
+    printf("Enter a number to find it's Factorial: ");//Enter the number for find the factorial
+    scanf("%d", &num);//The value of num is take by user
+    if (num < 0)//num is less than 0
+    {
+        printf("Factorial of negative number not possible\n");//factorial of negative number not possible
+    }
+    else
+    {
+        result = factorial(num);
+        printf("The Factorial of %d is %d.\n", num, result);//print the factorial of enter the number
+    }
+    return 0;//return statement
+}
+int factorial(int num)
+{
+    if (num == 0 || num == 1)//num is equal to o or 1
+    {
+        return 1;
+    }
+   else
+    {
+        return(num * factorial(num - 1));//num is multiply by factorial
+    }
+}  
