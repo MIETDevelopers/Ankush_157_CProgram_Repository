97da4d91a7895bdbf4ab6bfd7b0bddadf806d5dd
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-22 22:21:41 +0530

    Update 25_free.c

--- 25_free.c
+++ 25_free.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>  
 	int main() {
 	int* ptr = malloc(10 * sizeof(*ptr));//deallocates the memory previously allocated by a call to malloc
-		if (ptr != NULL){//if ptr is not equal to NuLL it pribt the value of integer
+		if (ptr != NULL){//if ptr is not equal to NuLL it print the value of integer
   		*(ptr + 2) = 50;
   		printf("Value of the 2nd integer is %d",*(ptr + 2));//print the output
 		}
