e2fd9a0fb743dd49ed03ea31894919786d2523cd
unknown <2020a1r157@mietjammu.in>
2021-03-26 10:17:13 +0530

    program to write the file

---
+++ 29_write.c
@@ -0,0 +1,25 @@
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+#include <stdlib.h>
+
+int main()//where the execution start from main function
+{
+   int num;//declare integer and its varaible num
+   FILE *fptr;//declare file
+
+   // use appropriate location if you are using MacOS or Linux
+   fptr = fopen("C:\\program.txt","w");
+
+   if(fptr == NULL)
+   {
+      printf("Error!");//if statement true it print 
+      exit(1);             
+   }
+
+   printf("Enter num: ");//Enter the number
+   scanf("%d",&num);//take input from user
+
+   fprintf(fptr,"%d",num);
+   fclose(fptr);
+
+   return 0;//return statement
+}
