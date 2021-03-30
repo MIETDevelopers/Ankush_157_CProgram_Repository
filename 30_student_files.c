6ce67ba65c6e6cd5f46adda737bcc04093a62045
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-30 18:41:16 +0530

    Program to read and write to store student marks  in  file

--- 30_student_files.c
+++ 30_student_files.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+#include <stdlib.h>//preprocessor directive to include library header file
 int main(){
-   char name[50];
-   int marks,i,n;
+   char name[50];//Declare of character and its array with 50 size
+   int marks,i,n;//Declare integer and its variable
    printf("Enter number of students:\n ");//Enter the number of student
-   scanf("%d",&n);
-   FILE *fptr;
-   fptr=(fopen("C:/Users/pc/Documents/student.txt","a"));
-   if(fptr==NULL){
+   scanf("%d",&n);//The value of n is taken by user
+   FILE *fptr;//Declare file
+   fptr=(fopen("C:/Users/pc/Documents/student.txt","a"));//location of csv file
+   if(fptr==NULL){//if fptr is null it print error
        printf("Error!");
        exit(1);
    }
-   for(i=0;i<n;++i)
+   for(i=0;i<n;++i)//loop start from  to 0 and n is taken by user
    {
       printf("For student%d\nEnter name: ",i+1);
-      scanf("%s",name);
+      scanf("%s",name);//name is taken by user
       printf("Enter marks:\n ");
-      scanf("%d",&marks);
+      scanf("%d",&marks);//marks are taken by user
       fprintf(fptr,"\nName: %s \nMarks=%d \n",name,marks);
    }
-   fclose(fptr);
-   return 0;
+   fclose(fptr);//close the file
+   return 0;//return statement
 }
