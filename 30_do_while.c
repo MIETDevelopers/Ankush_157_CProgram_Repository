aae09d27d4ca37e974a0c3077516edf93207e843
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-03-31 21:44:24 +0530

    do while program

--- 30_do_while.c
+++ 30_do_while.c
@@ -1,11 +1,13 @@
+//Author:Ankush kumar
+//Date:24/03/2021
 #include <stdio.h>//preprocessor directive to include standard input or output header file
-int main()//where the execution start from main function
+int main()//The execution start from main function
 {
 	int j=0;//Declare the integer and its variable or value
 	//start of do while loop
 	do
 	{
-		printf("Value of variable j is: %d\n", j);//print statemenet and then checked condition
+		printf("Value of variable j is: %d\n", j);//First execute and then checked condition
 		j++;
 	}while (j<=3);
 	return 0;//return statement
