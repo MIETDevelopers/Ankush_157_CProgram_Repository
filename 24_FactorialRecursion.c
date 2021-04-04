0a7a5c9f5dd4aaac6469b2b22551381a5e2679a7
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 23:05:57 +0530

    Factorial using recursion

--- 24_FactorialRecursion.c
+++ 24_FactorialRecursion.c
@@ -1,17 +1,36 @@
-//A C Program to print factorial using recursion.	 			© Ishav Verma 20/March/2021
-#include<stdio.h>
-	long int multiplyNumbers(int n);
-	int main() {
-    	int n;
-    	printf("Enter a positive integer: ");
-    	scanf("%d",&n);
-    	printf("Factorial of %d = %ld", n, multiplyNumbers(n));
-    	return 0;
-	}
-
-	long int multiplyNumbers(int n) {
-    	if (n>=1)
-        	return n*multiplyNumbers(n-1);
-    	else
-        	return 1;
+//Author:Ankush kumar
+//Date:26/03/2021
+//purpose of the program to find the factorial number with recursion
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+ 
+int factorial(int);
+ 
+int main()//The execution start from the main function
+{
+    int num;//declare num variable
+    int result;//declare result variable
+ 
+    printf("Enter a number to find it's Factorial: ");//Enter the number for find the factorial
+    scanf("%d", &num);//The value of num is take by user
+    if (num < 0)//num is less than 0
+    {
+        printf("Factorial of negative number not possible\n");//factorial of negative number not possible
+    }
+    else
+    {
+        result = factorial(num);
+        printf("The Factorial of %d is %d.\n", num, result);//print the factorial of enter the number
+    }
+    return 0;//return statement
 }
+int factorial(int num)
+{
+    if (num == 0 || num == 1)//num is equal to o or 1
+    {
+        return 1;
+    }
+   else
+    {
+        return(num * factorial(num - 1));//num is multiply by factorial
+    }
+}  
