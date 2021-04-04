cb01f4557efe72f6f42d9ce2c4c8e19592e2402b
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 23:02:04 +0530

    swap two number using call by value

--- 25_SwapCallByValue.c
+++ 25_SwapCallByValue.c
@@ -1,17 +1,32 @@
-//A C Program to swap two numbers using call by value.              © Ishav Verma 31/March/2021
-#include<stdio.h>
-void swap(int, int);
-int main(){
-    int a, b;
-    printf("Enter two values: ");
-    scanf("%d %d", &a, &b);//Taking input from user.
-    swap(a,b);
+//Author:Ankush kuamr
+//Date:26/03/2021
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+ 
+ 
+void swap(int, int);//declare swap function
+ 
+int main()
+{
+   int x, y;//declare  the variable x and y
+ 
+   printf("Enter the value of x and y\n");
+   scanf("%d%d",&x,&y);//The value of x and y is take by user
+ 
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y);//print orignal value of x and y
+ 
+   swap(x, y); 
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", x, y);//swaping of number 
+ 
+   return 0;
 }
-//Here swapping exists.
-void swap (int a, int b){
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-    printf("%d %d", a, b);
+ 
+void swap(int a, int b)
+{
+   int temp;//declare temp variable
+ 
+   temp = b;//The value of a is assigned to temp
+   b = a;//The value of b is assigned to a 
+   a = temp;//The value of temp is assigned to a
+    printf("Values of a and b is %d  %d\n",a,b);//Print the value of a and b
 }
