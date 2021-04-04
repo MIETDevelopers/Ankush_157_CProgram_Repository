0e0f333ce3a0689095c3fe70fd2602ea20004898
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 23:33:26 +0530

    Hello world program

--- 03_Hello.c
+++ 03_Hello.c
@@ -2,7 +2,7 @@
 //Date:23/03/2021
 //Make simple hello world program
 #include<stdio.h> //Including standard input ouput header files from library.
-int main(){ //The execution start from main funtion
+int main(){//The execution start from main funtion
 	printf("Hello world");//Here we entered the texted to be printed.
 	return 0;//Return statement
 } 
