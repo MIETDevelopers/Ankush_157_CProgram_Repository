f1306c0760d614a05c5c1ea76af45f13ecb3d0ee
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 23:00:00 +0530

    swap of two number using call by reference

--- 25_SwapCallByReference.c
+++ 25_SwapCallByReference.c
@@ -1,29 +1,31 @@
-//A C Program to swap two numbers using call by reference.              © Ishav Verma 20/March/2021
-#include <stdio.h>
+//Author:Ankush kumar
+//Date: 25/03/2021
+//purpose of the program is swaping of two number using call by reference
+#include <stdio.h>//pre processor directive to include standard input or output header file
  
-void swap(int*, int*);
+void swap(int*, int*);//swap fuction declare
  
-int main()
+int main()//The execution start from main function
 {
-   int x, y;
+   int x, y;//declare x,y variable
  
-   printf("Enter the value of x and y\n");
-   scanf("%d%d",&x,&y);
+   printf("Enter the value of x and y\n");// Enter the value of x and y
+   scanf("%d%d",&x,&y);//The value of x and y is take by user
  
-   printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y);//print orignal value of x and y
  
-   swap(&x, &y); 
+   swap(&x, &y); //
  
-   printf("After Swapping\nx = %d\ny = %d\n", x, y);
+   printf("After Swapping\nx = %d\ny = %d\n", x, y);//print swapped value of x and y
  
    return 0;
 }
  
-void swap(int *a, int *b)
+void swap(int *a, int *b)//function to swap two number
 {
    int temp;
-   // Here swapping occurs.
-   temp = *b;
-   *b = *a;
-   *a = temp;   
+ 
+   temp = *b;//The value of b is assigned to temp
+   *b = *a;//The value of  a is assigned to b
+   *a = temp; //The value of temp is assigned to a
 }
