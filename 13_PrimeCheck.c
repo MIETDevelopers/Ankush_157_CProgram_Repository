42bfe6412c0d4bd08086f8556f555d30052ccad9
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 22:30:56 +0530

    To check the number is prime or not using loop

--- 13_PrimeCheck.c
+++ 13_PrimeCheck.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-int main()
-{
-   int n, i, count = 0;
- 
-    printf("Enter the Number.\n");
-    scanf("%d",&n);//This will scan (read) the number entered by user.
- 
-    for(i=2; i<=n/2; ++i)
-    {
-        //Check for non prime number
-        if(n%i==0)
+//Author:Ankush kumar
+//Date:26/03/2021
+//purpose of the program is to check the number is prime or not
+#include<stdio.h>//preprocessor directive to include standard input or outpur header file
+int main(){//The execution start from main function
+    int a,i=2,f=1;//declare the variable
+    printf("Enter a number:\n");//enter the number
+    scanf("%d",&a);// the input of a is take by user
+    while(i<a) //in the program,loop iterated from i<=a/2
+    {   //condition for non prime
+        if(a%i == 0)// if a is perfectly divisible by1 is checked using this statment
         {
-            count=1;
-            break;
+            f=0;
+            break;//If n is perfectly divisible by i, a is not a prime number. In this case, f is set to true, and the loop is terminated using the break statement.
         }
+        i++;
     }
-    //This will give the output.
-    if (count==0)
-        printf("%d is a Prime number.",n);
-    else
-        printf("%d is not a Prime number.",n);
- 
-    getchar();    
+    if(f)
+         printf("Prime Number=%d\n",a);//print prime number
+        
+    else { 
+         printf("Not Prime Number=%d\n",a);//print not prime number
+         }
+    
+    return 0;//return statement
 }
