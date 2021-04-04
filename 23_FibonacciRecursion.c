842568ead8618109dd95c7decd0b805b5c506bf2
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 22:58:10 +0530

    Fibonacci series using recursion

--- 23_FibonacciRecursion.c
+++ 23_FibonacciRecursion.c
@@ -1,7 +1,9 @@
-//A C Program to print Fibonacci series using recursion.	 			© Ishav Verma 20/March/2021
-#include<stdio.h>    
+//Author:Ankush kumar
+//Date:26/03/2021
+//Purpose of the Program to print Fibonacci series using recursion.
+#include<stdio.h>//preprocessor directive to include standard input or output header file    
 int main(){    
-	int n1=0,n2=1,n3,i,number;    
+	int n1=0,n2=1,n3,i,number;//declare of variable and its value    
 	printf("Enter the number of elements: ");    
 	scanf("%d",&number);    
 	printf(" %d %d",n1,n2);//Printing 0 and 1.   
@@ -12,5 +14,5 @@ int main(){
   			n1=n2;    
   			n2=n3;    
  		}  
-	return 0;  
+	return 0;  //return statement
 }
