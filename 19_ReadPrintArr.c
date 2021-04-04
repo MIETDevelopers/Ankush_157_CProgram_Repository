741bb9a2bd513ab91e89c8718762380d5dcd1750
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 22:41:57 +0530

    To read and print element in two dimensional array

--- 19_ReadPrintArr.c
+++ 19_ReadPrintArr.c
@@ -1,14 +1,16 @@
-// A C Program to read and print elements in two dimensional array. © Ishav Verma 19/March/2021
-#include<stdio.h>
+//Author:Ankush kumar
+//Date;26/03/2021
+//Purpose of the Program to read and print elements in two dimensional array.
+#include<stdio.h>//preprocessor directive to include standard input or output header file
 int main(){
    /* 2D array declaration*/
    int disp[3][4];
    /*Counter variables for the loop*/
    int i, j;
-   for(i=0; i<3; i++) {
+   for(i=0; i<3; i++) {//loop start from to 0 and end at 3
       for(j=0;j<4;j++) {
          printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &disp[i][j]);
+         scanf("%d", &disp[i][j]);//The value of dis[i][j] is taken by user
       }
    }
    //Displaying array elements
@@ -17,9 +19,9 @@ int main(){
       for(j=0;j<4;j++) {
          printf("%d \n", disp[i][j]);
          if(j==4){
-            printf("\n");
+            printf("\n");//print the output
          }
       }
    }
-   return 0;
+   return 0;//return statement
 } 
