75403c4b1e2efa310c37a7cefd8ea81ca100f901
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 22:54:45 +0530

    To make simple calculator using switch case

--- 22_Calculator.c
+++ 22_Calculator.c
@@ -1,29 +1,31 @@
-#include<stdio.h>
-int main(){
-    char operator;
-    double a, b;
-    printf("Enter an operator (+, -, *, /):"); //It will print the operater to be entered by user.
-    scanf("%c", &operator); //It will scan the operater entered by user.
-    printf("Enter two operands:");
-    scanf("%lf %lf",&a, &b); //It will scan the numbers entered by user.
-    switch(operator) //Switch case is used to perfom the calculation case that is scanned (in line 6) based on user input.
-    {
-        case '+':
-            printf("%.2lf + %.2lf = %.2lf",a, b, a+b); //Addition Function
-            break;
+//Author:Ankush kumar
+//Date:26/03/2021
+#include <stdio.h>//preprocessor directive to include standard input or output header file
+int main() {
+    char operator;//Declare charactar and operator variable
+    float first, second;
+    printf("Enter an operator (+, -, *,): ");//Take all operator
+    scanf("%c", &operator);//The value of operator is take by user
+    printf("Enter two operands: ");
+    scanf("%f %f", &first, &second);//The value of first and second is take by user
 
-        case '-':
-            printf("%.2lf - %.2lf = %.2lf",a, b, a-b); //Subtraction Function
-            break;
-
-        case '*':
-            printf("%.2lf * %.2lf = %.2lf",a, b, a*b); //Multiplication Function
-            break;
-
-        case '/':
-            printf("%.2lf / %.2lf = %.2lf",a, b, a/b); //Division Function
-            break;
-    printf("Error encountered by the input values");  //Default
+    switch (operator) {//Take a switch statement
+    case '+'://case for addition
+        printf("%f + %f = %f", first, second, first + second);//Addition of two number
+        break;//if the condition is true it jump to switch statement
+    case '-':
+        printf("%f - %f = %f", first, second, first - second);//Subtration of two number
+        break;
+    case '*':
+        printf("%f * %f = %f", first, second, first * second);//Multiplication of two number
+        break;
+    case '/':
+        printf("%f / %f = %f", first, second, first / second);//division of two number
+        break;
+        // operator doesn't match any case constant
+    default://if the all case are not true it print the Error
+        printf("Error! operator is not correct");
     }
+
     return 0;
 }
