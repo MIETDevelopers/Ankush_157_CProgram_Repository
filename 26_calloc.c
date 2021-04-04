b558b8034be296200bc98d9b709cde9c58458818
ankush1290 <75369520+ankush1290@users.noreply.github.com>
2021-04-04 23:10:26 +0530

    To demonstrate the use of calloc()

--- 26_calloc.c
+++ 26_calloc.c
@@ -1,18 +1,20 @@
-// A C Program demonstrating use of calloc() function.          © Ishav Verma 20/March/2021
-#include <stdio.h> //Using standard input output header file.
-#include <stdlib.h> //Using standard library header file.
+//Author:Ankush kumar
+//Date:26/03/2021
+// Purpose of the Program demonstrating use of calloc() function.
+#include <stdio.h> //preprocessor directive to include standard input or output header file
+#include <stdlib.h>//
     int main() {
-        int i, * ptr, sum = 0;
+        int i, * ptr, sum = 0;//declare the integer or its variable
         ptr = calloc(10, sizeof(int));
         if (ptr == NULL) {
-            printf("Error! memory not allocated.");
+            printf("Error! memory not allocated.");//print Error memory not allocate
             exit(0);
         }
-        printf("Building and calculating the sequence sum of the first 10 terms \n ");
-        for (i = 0; i < 10; ++i) { * (ptr + i) = i;
+        printf("Building and calculating the sequence sum of the first 10 terms \n ");//print thr sequencd sum of the first 10 term
+        for (i = 0; i < 10; ++i) { * (ptr + i) = i;//loop start from 0 and less than 10
             sum += * (ptr + i);
         }
-        printf("Sum = %d", sum);
+        printf("Sum = %d", sum);//print the sum output
         free(ptr);
-        return 0;
-    }
+        return 0;//return statement
+    } 
